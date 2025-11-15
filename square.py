870f40dce6ba0c928d43906b3d4b59487a11500c
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-11-15 10:34:03 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("Aqua")
+board=turtle.Turtle()
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
